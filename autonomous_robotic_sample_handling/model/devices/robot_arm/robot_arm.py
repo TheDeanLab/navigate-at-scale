4d77836e1db75e5d11fb8f42df1aa4e2321d755e
Sai Bommisetty <EWizard512@gmail.com>
2024-01-30 17:27:33 -0600

    Setup environment for robot arm connection

--- autonomous_robotic_sample_handling/model/devices/robot_arm/robot_arm.py
+++ autonomous_robotic_sample_handling/model/devices/robot_arm/robot_arm.py
@@ -1,3 +1,19 @@
+import mecademicpy.robot as mdr
+
+def connect_to_robot_arm(configuration):
+    robot = mdr.Robot()
+    print("Attempting connection")
+    robot.Connect(address='192.168.0.100', enable_synchronous_mode=True)
+    start_robot_arm(robot, None)
+    return robot
+
+def start_robot_arm(device: mdr.Robot, configuration):
+    print("Activating and Homing robot arm")
+    device.Move
+    device.ActivateAndHome()
+    device.Delay(2)
+    device.Disconnect()
+
 class RobotArm:
     """A Robot Arm class"""
     def __init__(self, device_connection, *args):
@@ -12,11 +28,10 @@ class RobotArm:
         """
         self.device_connection = device_connection
 
-    def move(self, robot):
-        """Move the custom device"""
+    def zero_joints(self, robot):
+        """Zero the joints"""
         print("Custom device is going to zero joints")
         robot.MoveJoints(0, 0, 0, 0, 0, 0)
-        robot.Delay(15)
         print('Robot has completed movement')
 
     @property
