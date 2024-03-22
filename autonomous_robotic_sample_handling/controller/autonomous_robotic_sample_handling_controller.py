f146e56dfbe5227eec0fcd2d6169c5a23953ebbe
Abhijeet Kulkarni <137450031+abhikulkarni2002@users.noreply.github.com>
2024-03-22 18:06:40 -0500

    Created relative config to allow for automatic routine generation.
    Co-authored-by: Sai Bommisetty <EWizard512@gmail.com>

--- autonomous_robotic_sample_handling/controller/autonomous_robotic_sample_handling_controller.py
+++ autonomous_robotic_sample_handling/controller/autonomous_robotic_sample_handling_controller.py
@@ -1,4 +1,6 @@
 from navigate.tools.file_functions import load_yaml_file
+from navigate.config.config import get_navigate_path
+
 
 from autonomous_robotic_sample_handling.controller.sub_controllers.robot_arm_controller import (
     RobotArmController
@@ -29,9 +31,11 @@ class AutonomousRoboticSampleHandlingController:
         self.buttons = self.view.buttons
 
         self.buttons['automation_sequence'].configure(command=self.automated_sample_handling)
-        self.buttons['process_sample'].configure(command=self.sample_iteration)
+        self.buttons['process_sample'].configure(command=self.move_robot_arm_to_loading_zone)
         self.buttons['offline_program'].configure(command=self.start_offline_program)
-      
+        
+        self.data = self.load_config_data()
+        self.motor_position = self.get_motor_position(self.data)
 
         #Initialize sub-controllers
         self.robot_arm_controller = RobotArmController(
@@ -50,21 +54,55 @@ class AutonomousRoboticSampleHandlingController:
             self.view.move_sequence, self.parent_controller
         )
 
+    def load_config_data(self):
+        import os
+        plugins_config_path = os.path.join(
+        get_navigate_path(), "config", "plugins_config.yml"
+        )
+        plugins_config = load_yaml_file(plugins_config_path)
+        plugin_path = plugins_config["Autonomous Robotic Sample Handling"] 
+        local_config_path = os.path.join(plugin_path, "config", "configuration.yaml")
+        data = load_yaml_file(local_config_path)
+        return data
     
     def start_offline_program(self):
         self.parent_controller.execute(
             "start_program", "vertical_oscillation"
         )
 
-    def get_positions(self):
-        data = load_yaml_file('C:/Users/abhik/Documents/Senior Design/navigate-at-scale/autonomous_robotic_sample_handling/config/configuration.yaml')
-        robot_position = data['environment']['robot']['trf']
-        return robot_position
+    def get_motor_position(self, data):
+        motor_position = data["environment"]["motor"]["units"]
+        motor_to_robot_mm = motor_position*25.4
+        #for standard TRF, z is forwards, x is down
+        data_points = {
+            "robot_base": data["environment"]["robot"]["robot_base_plate_height"],
+            "vial_height": data["environment"]["components"]["vial_height_from_carousel"],
+            "carousel_height": data["environment"]["components"]["carousel_height_from_table"],
+            "vial_attack": data["environment"]["components"]["vial_attack_distance_from_carousel"],
+            "wrf_to_trf": data["environment"]["components"]["trf_from_wrf_cartesian"],
+            "trf_gripper_edge": data["environment"]["components"]["trf_to_gripper_edge"],
+            "trf_gripper_bottom": data["environment"]["components"]["trf_to_gripper_bottom"],
+            "MEPG_thickness": data["environment"]["components"]["MEPG_thickness"],
+            "tolerance_up": data["environment"]["components"]["tolerance_up"],
+            "tolerance_forward": data["environment"]["components"]["tolerance_forward"],
+        }
+        print(data_points["robot_base"],data_points["wrf_to_trf"][2],data_points["carousel_height"],data_points["vial_height"])
+        print(motor_to_robot_mm, data_points["vial_attack"], data_points["wrf_to_trf"][0], data_points['tolerance_forward'], data_points['trf_gripper_edge'])
+        motor_position_x = data_points["robot_base"] + data_points["wrf_to_trf"][2] - data_points["carousel_height"] - data_points["vial_height"] - data_points['trf_gripper_bottom'] -data_points['tolerance_up']
+        motor_position_z = motor_to_robot_mm - data_points["vial_attack"] - data_points["wrf_to_trf"][0] - data_points['tolerance_forward'] - data_points["MEPG_thickness"]
+        print(motor_to_robot_mm - data_points["vial_attack"] - data_points["wrf_to_trf"][0] - data_points['tolerance_forward'] - data_points['trf_gripper_edge'])
+        self.key_positions = {
+            "motor_position": [motor_position_x, 0, motor_position_z, 0, 0, 0],
+            "engage_header_distance": data_points['trf_gripper_edge'] - data_points['MEPG_thickness']
+        }
+        print(self.key_positions)
 
     def move_robot_arm_to_loading_zone(self):
-        
-        #change inputs to refer to config file
-        self.robot_arm_controller.move_lin(275, -1.5, 141.5, 0, 90, 0)
+        #TODO: change inputs to refer to config file
+        goal_pos = self.key_positions['motor_position']
+        x, y, z, Rx, Ry, Rz = goal_pos
+        z = z - self.key_positions['engage_header_distance']
+        self.robot_arm_controller.move_lin_rel_trf(x, y, z, Rx, Ry, Rz)
         print("Moving robot arm to loading zone")
         
     def engageHeader(self):
