2ad8e4551673c1d1ab1e56bee87679d10cf071d2
Kevin Dean <42547789+AdvancedImagingUTSW@users.noreply.github.com>
2024-06-24 09:53:37 -0500

    Update autonomous_robotic_sample_handling_controller.py

--- autonomous_robotic_sample_handling/controller/autonomous_robotic_sample_handling_controller.py
+++ autonomous_robotic_sample_handling/controller/autonomous_robotic_sample_handling_controller.py
@@ -1,3 +1,33 @@
+# Copyright (c) 2021-2024  The University of Texas Southwestern Medical Center.
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted for academic and research use only
+# (subject to the limitations in the disclaimer below)
+# provided that the following conditions are met:
+
+#      * Redistributions of source code must retain the above copyright notice,
+#      this list of conditions and the following disclaimer.
+
+#      * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+
+#      * Neither the name of the copyright holders nor the names of its
+#      contributors may be used to endorse or promote products derived from this
+#      software without specific prior written permission.
+
+# NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY
+# THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 # Standard Library Imports
 import os
@@ -23,6 +53,68 @@ from autonomous_robotic_sample_handling.controller.sub_controllers.inhouse_tools
     InHouseToolsPopupController
 )
 from autonomous_robotic_sample_handling.view.popups.inhouse_tools_popup import InHouseToolsPopup
+
+
+def load_config_data():
+    """ Load the configuration data
+
+    Returns
+    -------
+
+    """
+    plugins_config_path = os.path.join(
+        get_navigate_path(), "config", "plugins_config.yml"
+    )
+    plugins_config = load_yaml_file(plugins_config_path)
+    plugin_path = plugins_config["Autonomous Robotic Sample Handling"]
+    local_config_path = os.path.join(plugin_path, "config", "configuration.yaml")
+    data = load_yaml_file(local_config_path)
+    return data
+
+
+def get_microscope_position(data):
+    """ Get the microscope position
+
+    Parameters
+    ----------
+    data : dict
+        The data from the configuration
+
+    Returns
+    -------
+    list
+        The list of the positions
+    """
+    x = data["environment"]["microscope"]["x"]
+    y = data["environment"]["microscope"]["y"]
+    z = data["environment"]["microscope"]["z"]
+    theta_x = data["environment"]["microscope"]["R1"]
+    theta_y = data["environment"]["microscope"]["R2"]
+    theta_z = data["environment"]["microscope"]["R3"]
+    return [x, y, z, theta_x, theta_y, theta_z]
+
+
+def get_loading_zone_position(data):
+    """ Get the loading zone position
+
+    Parameters
+    ----------
+    data : dict
+        The data from the configuration
+
+    Returns
+    -------
+    list
+        The list of the positions
+    """
+    x = data["environment"]["loading_zone"]["pose"]["x"]
+    y = data["environment"]["loading_zone"]["pose"]["y"]
+    z = data["environment"]["loading_zone"]["pose"]["z"]
+    theta_x = data["environment"]["loading_zone"]["pose"]["Rx"]
+    theta_y = data["environment"]["loading_zone"]["pose"]["Ry"]
+    theta_z = data["environment"]["loading_zone"]["pose"]["Rz"]
+    flag = data["environment"]["loading_zone"]["flag"]
+    return [x, y, z, theta_x, theta_y, theta_z] if flag else None
 
 
 class AutonomousRoboticSampleHandlingController:
@@ -58,10 +150,10 @@ class AutonomousRoboticSampleHandlingController:
         self.buttons['offline_program'].configure(
             command=self.start_offline_program)
         self.buttons['in_house'].configure(
-            command=self.launch_inhouse_tools)
+            command=self.launch_in_house_tools)
 
         #: dict: The data from the configuration file
-        self.data = self.load_config_data()
+        self.data = load_config_data()
         self.prepare_config_data(self.data)
 
         # Initialize sub-controllers
@@ -86,12 +178,12 @@ class AutonomousRoboticSampleHandlingController:
         )
 
         #: InHouseToolsPopupController: The in house tools popup controller
-        self.inhouse_tools_popup_controller = None
+        self.in_house_tools_controller = None
 
         #: dict: The key positions
         self.key_positions = None
 
-    def launch_inhouse_tools(self):
+    def launch_in_house_tools(self):
         """Launches inhouse tools popup.
 
         Will only launch when button in GUI is pressed, and will not duplicate.
@@ -99,26 +191,12 @@ class AutonomousRoboticSampleHandlingController:
         """
 
         if hasattr(self, "inhouse_tools_popup_controller"):
-            self.inhouse_tools_popup_controller.showup()
+            self.in_house_tools_controller.showup()
             return
-        inhouse_tools = InHouseToolsPopup(self.view)
-        self.inhouse_tools_popup_controller = InHouseToolsPopupController(inhouse_tools, self)
-
-    def load_config_data(self):
-        """ Load the configuration data
-
-        Returns
-        -------
-
-        """
-        plugins_config_path = os.path.join(
-            get_navigate_path(), "config", "plugins_config.yml"
+        in_house_tools = InHouseToolsPopup(self.view)
+        self.in_house_tools_controller = InHouseToolsPopupController(
+            in_house_tools, self
         )
-        plugins_config = load_yaml_file(plugins_config_path)
-        plugin_path = plugins_config["Autonomous Robotic Sample Handling"]
-        local_config_path = os.path.join(plugin_path, "config", "configuration.yaml")
-        data = load_yaml_file(local_config_path)
-        return data
 
     def start_offline_program(self, program_name="Vertical_Oscillation"):
         """ Start the offline program
@@ -132,49 +210,6 @@ class AutonomousRoboticSampleHandlingController:
             "start_program", program_name
         )
 
-    def get_microscope_position(self, data):
-        """ Get the microscope position
-
-        Parameters
-        ----------
-        data : dict
-            The data from the configuration
-
-        Returns
-        -------
-        list
-            The list of the positions
-        """
-        x = data["environment"]["microscope"]["x"]
-        y = data["environment"]["microscope"]["y"]
-        z = data["environment"]["microscope"]["z"]
-        theta_x = data["environment"]["microscope"]["R1"]
-        theta_y = data["environment"]["microscope"]["R2"]
-        theta_z = data["environment"]["microscope"]["R3"]
-        return [x, y, z, theta_x, theta_y, theta_z]
-
-    def get_loading_zone_position(self, data):
-        """ Get the loading zone position
-
-        Parameters
-        ----------
-        data : dict
-            The data from the configuration
-
-        Returns
-        -------
-        list
-            The list of the positions
-        """
-        x = data["environment"]["loading_zone"]["pose"]["x"]
-        y = data["environment"]["loading_zone"]["pose"]["y"]
-        z = data["environment"]["loading_zone"]["pose"]["z"]
-        theta_x = data["environment"]["loading_zone"]["pose"]["Rx"]
-        theta_y = data["environment"]["loading_zone"]["pose"]["Ry"]
-        theta_z = data["environment"]["loading_zone"]["pose"]["Rz"]
-        flag = data["environment"]["loading_zone"]["flag"]
-        return [x, y, z, theta_x, theta_y, theta_z] if flag else None
-
     def prepare_config_data(self, data):
         """ Prepare the configuration data
 
@@ -187,41 +222,59 @@ class AutonomousRoboticSampleHandlingController:
         motor_to_robot_mm = motor_position * 25.4
         # for standard TRF, z is forwards, x is down
         data_points = {
-            "robot_base": data["environment"]["robot"]["robot_base_plate_height"],
-            "vial_height": data["environment"]["components"]["vial_height_from_carousel"],
-            "carousel_height": data["environment"]["components"]["carousel_height_from_table"],
-            "vial_attack": data["environment"]["components"]["vial_attack_distance_from_carousel"],
-            "wrf_to_trf": data["environment"]["components"]["trf_from_wrf_cartesian"],
-            "trf_gripper_edge": data["environment"]["components"]["trf_to_gripper_edge"],
-            "trf_gripper_bottom": data["environment"]["components"]["trf_to_gripper_bottom"],
-            "MEPG_thickness": data["environment"]["components"]["MEPG_thickness"],
-            "sample_height": data["environment"]['components']['sample_height'],
-            "shear_distance": data["environment"]['components']['shear_distance'],
-            "tolerance_up": data["environment"]["components"]["tolerance_up"],
-            "tolerance_forward": data["environment"]["components"]["tolerance_forward"],
+            "robot_base": data[
+                "environment"]["robot"]["robot_base_plate_height"],
+            "vial_height": data[
+                "environment"]["components"]["vial_height_from_carousel"],
+            "carousel_height": data[
+                "environment"]["components"]["carousel_height_from_table"],
+            "vial_attack": data[
+                "environment"]["components"]["vial_attack_distance_from_carousel"],
+            "wrf_to_trf": data[
+                "environment"]["components"]["trf_from_wrf_cartesian"],
+            "trf_gripper_edge": data[
+                "environment"]["components"]["trf_to_gripper_edge"],
+            "trf_gripper_bottom": data[
+                "environment"]["components"]["trf_to_gripper_bottom"],
+            "MEPG_thickness": data[
+                "environment"]["components"]["MEPG_thickness"],
+            "sample_height": data[
+                "environment"]['components']['sample_height'],
+            "shear_distance": data[
+                "environment"]['components']['shear_distance'],
+            "tolerance_up": data[
+                "environment"]["components"]["tolerance_up"],
+            "tolerance_forward": data[
+                "environment"]["components"]["tolerance_forward"],
         }
         motor_position_x = data_points["robot_base"] + data_points[
             "wrf_to_trf"][2] - data_points["carousel_height"] - data_points[
             "vial_height"] - data_points['trf_gripper_bottom'] + data_points[
             'tolerance_up']
 
-        motor_position_z = motor_to_robot_mm - data_points["vial_attack"] - data_points["wrf_to_trf"][0] - data_points[
+        motor_position_z = motor_to_robot_mm - data_points[
+            "vial_attack"] - data_points["wrf_to_trf"][0] - data_points[
             'tolerance_forward'] - data_points["MEPG_thickness"]
 
-        loading_zone_x = motor_to_robot_mm - data_points['vial_attack'] - data_points['tolerance_forward'] - \
+        loading_zone_x = motor_to_robot_mm - data_points[
+            'vial_attack'] - data_points['tolerance_forward'] - \
                          data_points['MEPG_thickness']
 
-        loading_zone_z = data_points['carousel_height'] + data_points['vial_height'] + data_points[
-            'trf_gripper_bottom'] - data_points['robot_base'] + data_points["tolerance_up"]
+        loading_zone_z = data_points['carousel_height'] + data_points[
+            'vial_height'] + data_points[
+            'trf_gripper_bottom'] - data_points[
+            'robot_base'] + data_points["tolerance_up"]
 
         self.key_positions = {
             "motor_position": [motor_position_x, 0, motor_position_z, 0, 0, 0],
             "loading_zone": [loading_zone_x, 0, loading_zone_z, 0, 90, 0],
-            "engage_header_distance": data_points['trf_gripper_edge'] - data_points['MEPG_thickness'],
+            "engage_header_distance": data_points[
+                                          'trf_gripper_edge'] - data_points[
+                'MEPG_thickness'],
             "shear_distance": data_points['shear_distance'],
             "sample_height": data_points['sample_height'],
-            "microscope": self.get_microscope_position(data),
-            "loading_zone_manual": self.get_loading_zone_position(data),
+            "microscope": get_microscope_position(data),
+            "loading_zone_manual": get_loading_zone_position(data),
         }
 
     def move_robot_arm_to_loading_zone(self):
@@ -230,13 +283,13 @@ class AutonomousRoboticSampleHandlingController:
         engage_header_distance = self.key_positions['engage_header_distance']
         if loading_zone_manual is None:
             loading_zone = self.key_positions['loading_zone']
-            x, y, z, Rx, Ry, Rz = loading_zone
+            x, y, z, rot_x, rot_y, rot_z = loading_zone
             self.robot_arm_controller.move_lin(
-                x - engage_header_distance, y, z, Rx, Ry, Rz)
+                x - engage_header_distance, y, z, rot_x, rot_y, rot_z)
         else:
-            x, y, z, Rx, Ry, Rz = loading_zone_manual
+            x, y, z, rot_x, rot_y, rot_z = loading_zone_manual
             self.robot_arm_controller.move_lin(
-                x - engage_header_distance, y, z, Rx, Ry, Rz)
+                x - engage_header_distance, y, z, rot_x, rot_y, rot_z)
         self.engage_header()
         self.remove_header_from_stage()
 
@@ -244,7 +297,8 @@ class AutonomousRoboticSampleHandlingController:
         """ Engage the header. """
         self.robot_arm_controller.open_gripper()
         engage_header_distance = self.key_positions["engage_header_distance"]
-        self.robot_arm_controller.move_lin_rel_trf(0, 0, engage_header_distance, 0, 0, 0)
+        self.robot_arm_controller.move_lin_rel_trf(
+            0, 0, engage_header_distance, 0, 0, 0)
         self.robot_arm_controller.delay(1)
         self.robot_arm_controller.close_gripper()
 
@@ -259,13 +313,18 @@ class AutonomousRoboticSampleHandlingController:
     def move_to_microscope(self):
         """ Move to the microscope. """
         microscope = self.key_positions['microscope']
-        x, y, z, Rx, Ry, Rz = microscope
+        x, y, z, rot_x, rot_y, rot_z = microscope
         microscope_tolerance = 10
         engage_header_distance = self.key_positions['engage_header_distance']
-        self.robot_arm_controller.move_lin(x, y + engage_header_distance, z - microscope_tolerance, Rx, Ry, Rz)
-        self.engage_microscope(microscope_tolerance=microscope_tolerance, engage_header_distance=engage_header_distance)
-        self.disengage_microscope(engage_header_distance=engage_header_distance)
-        self.remove_header_from_microscope(engage_header_distance=engage_header_distance)
+        self.robot_arm_controller.move_lin(x, y + engage_header_distance, z -
+                                           microscope_tolerance, rot_x, rot_y, rot_z)
+        self.engage_microscope(
+            microscope_tolerance=microscope_tolerance,
+            engage_header_distance=engage_header_distance)
+        self.disengage_microscope(
+            engage_header_distance=engage_header_distance)
+        self.remove_header_from_microscope(
+            engage_header_distance=engage_header_distance)
 
     def engage_microscope(self, microscope_tolerance, engage_header_distance):
         """ Engage the microscope
@@ -306,11 +365,14 @@ class AutonomousRoboticSampleHandlingController:
         """
         shear_distance = self.key_positions['shear_distance']
         z_tolerance = 50
-        self.robot_arm_controller.move_lin_rel_trf(0, 0, engage_header_distance * 2, 0, 0, 0)
+        self.robot_arm_controller.move_lin_rel_trf(
+            0, 0, engage_header_distance * 2, 0, 0, 0)
         self.robot_arm_controller.close_gripper()
         self.robot_arm_controller.delay(1)
-        self.robot_arm_controller.move_lin_rel_trf(0, -shear_distance * 0.75, 0, 0, 0, 0)
-        self.robot_arm_controller.move_lin_rel_trf(0, 0, -z_tolerance, 0, 0, 0)
+        self.robot_arm_controller.move_lin_rel_trf(
+            0, -shear_distance * 0.75, 0, 0, 0, 0)
+        self.robot_arm_controller.move_lin_rel_trf(
+            0, 0, -z_tolerance, 0, 0, 0)
         self.robot_arm_controller.zero_joints()
 
     def return_header_to_carousel(self):
@@ -321,23 +383,28 @@ class AutonomousRoboticSampleHandlingController:
         engage_header_distance = self.key_positions['engage_header_distance']
         if loading_zone_manual is None:
             loading_zone = self.key_positions['loading_zone']
-            x, y, z, Rx, Ry, Rz = loading_zone
-            self.robot_arm_controller.move_lin(x, y, z + sample_height, Rx, Ry, Rz)
+            x, y, z, rot_x, rot_y, rot_z = loading_zone
+            self.robot_arm_controller.move_lin(
+                x, y, z + sample_height, rot_x, rot_y, rot_z
+            )
         else:
-            x, y, z, Rx, Ry, Rz = loading_zone_manual
-            self.robot_arm_controller.move_lin(x, y, z + sample_height, Rx, Ry, Rz)
+            x, y, z, rot_x, rot_y, rot_z = loading_zone_manual
+            self.robot_arm_controller.move_lin(
+                x, y, z + sample_height, rot_x, rot_y, rot_z
+            )
 
         # Move sample above loading zone
-        # self.robot_arm_controller.move_lin(x, y, z + sample_height, Rx, Ry, Rz)
         self.robot_arm_controller.delay(1)
 
         # Place sample within vial
-        self.robot_arm_controller.move_lin_rel_trf(sample_height, 0, 0, 0, 0, 0)
+        self.robot_arm_controller.move_lin_rel_trf(
+            sample_height, 0, 0, 0, 0, 0)
         self.robot_arm_controller.open_gripper()
         self.robot_arm_controller.delay(1)
 
         # Disengage robot arm from loading zone
-        self.robot_arm_controller.move_lin_rel_trf(0, 0, -engage_header_distance, 0, 0, 0)
+        self.robot_arm_controller.move_lin_rel_trf(
+            0, 0, -engage_header_distance, 0, 0, 0)
 
     def sample_iteration(self):
         """ Sample iteration. """
@@ -352,10 +419,9 @@ class AutonomousRoboticSampleHandlingController:
         self.automation_controller.reset_automation_variables()
         num_samples = self.automation_controller.get_num_samples()
         og_position = 9.15
-        dtheta = 15
+        delta_theta = 15
         for i in range(1, num_samples + 1):
             self.motor_controller.MoveTo(og_position)
             self.sample_iteration()
-            print(f"Finished processing sample {i}")
-            og_position = og_position + dtheta
+            og_position = og_position + delta_theta
             self.automation_controller.update_progress_bar(i)
