be94fb857158a839f366f3725773210ef6f64993
Kevin Dean <42547789+AdvancedImagingUTSW@users.noreply.github.com>
2024-06-24 12:39:04 -0500

    update view

--- autonomous_robotic_sample_handling/view/frames/robot_initialization.py
+++ autonomous_robotic_sample_handling/view/frames/robot_initialization.py
@@ -1,3 +1,34 @@
+# Copyright (c) 2021-2024  The University of Texas Southwestern Medical Center.
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted for academic and research use only
+# (subject to the limitations in the disclaimer below)
+# provided that the following conditions are met:
+
+#      * Redistributions of source code must retain the above copyright notice,
+#      this list of conditions and the following disclaimer.
+
+#      * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+
+#      * Neither the name of the copyright holders nor the names of its
+#      contributors may be used to endorse or promote products derived from this
+#      software without specific prior written permission.
+
+# NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY
+# THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 import tkinter as tk
 from tkinter import ttk
 
@@ -7,44 +38,38 @@ class RobotInitialization(tk.Frame):
 
     RobotInitialization is a frame that contains the widgets for initializing
     robot movement.
-
-    Parameters
-    ----------
-    settings_tab : tk.Frame
-        The frame that contains the settings tab.
-    *args : tuple
-        Variable length argument list.
-    **kwargs : dict
-        Arbitrary keyword arguments.
-
-    Attributes
-    ----------
-    buttons : dict
-        A dictionary of all the buttons that are tied to each widget name.
-        The key is the widget name, value is the button associated.
-
-    Methods
-    -------
     """
 
     def __init__(self, settings_tab, *args, **kwargs):
-        text_label = 'Robot Initialization'
-        ttk.Labelframe.__init__(self, settings_tab, text=text_label, *args, **kwargs)
+        """Initialize the RobotInitialization frame
 
-        # Formatting
+        Parameters
+        ----------
+        settings_tab : tk.Frame
+            The frame that contains the settings tab.
+        *args : tuple
+            Variable length argument list.
+        **kwargs : dict
+            Arbitrary keyword arguments.
+
+        """
+        text_label = "Robot Initialization"
+        ttk.Labelframe.__init__(self, settings_tab, text=text_label, *args, **kwargs)
         tk.Grid.columnconfigure(self, "all", weight=1)
         tk.Grid.rowconfigure(self, "all", weight=1)
 
-        # Initializing Button
+        #: dict: A dictionary of buttons for initializing the robot.
         self.buttons = {
             "import": ttk.Button(self, text="Load Positions from Disk"),
             "connect": ttk.Button(self, text="Connect Robot"),
             "disconnect": ttk.Button(self, text="Disconnect Robot"),
-            "move": ttk.Button(self, text='Move Robot'),
+            "move": ttk.Button(self, text="Move Robot"),
             "zero": ttk.Button(self, text="Zero Joints"),
             "opengripper": ttk.Button(self, text="Open Gripper"),
             "closegripper": ttk.Button(self, text="Close Gripper"),
-            "movetoloadingzone": ttk.Button(self, text="Move Stage to Initial Loading Zone")
+            "movetoloadingzone": ttk.Button(
+                self, text="Move Stage to Initial Loading Zone"
+            ),
         }
         counter = 0
         for key, button in self.buttons.items():
@@ -72,4 +97,11 @@ class RobotInitialization(tk.Frame):
             counter += 1
 
     def get_buttons(self):
+        """Get the buttons
+
+        Returns
+        -------
+        dict
+            A dictionary of buttons for initializing the robot.
+        """
         return self.buttons
